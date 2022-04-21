8d8d4a4e5dfa6d1bbd3469924b94ff8c7c19975e
yuxuanwu17 <yuxuan.wu17@gmail.com>
2022-04-21 15:01:48 -0400

    unwanted error occurs

--- model.py
+++ model.py
@@ -62,7 +62,67 @@ class WeakRM(nn.Module):
         return bag_probability, gated_attention
 
 
+class WeakRMLSTM(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(13, 16, kernel_size=(1, 15), padding=(0, 7)),
+            nn.ReLU()
+        )
+
+        self.pool1 = nn.MaxPool2d((1, 2))
+
+        self.lstm = nn.LSTM(input_size=16, hidden_size=16, bidirectional=True, batch_first=True)
+
+        self.attention_v = nn.Sequential(
+            nn.Linear(32, 128),
+            nn.Tanh()
+        )
+
+        self.attention_u = nn.Sequential(
+            nn.Linear(32, 128),
+            nn.Sigmoid()
+        )
+
+        self.attention_weights = nn.Sequential(
+            nn.Linear(128, 1),
+            nn.Softmax()
+        )
+
+        self.cls = nn.Sequential(
+            nn.Linear(32, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, inputs, training=True, mask=None):
+        # inputs = torch.squeeze(inputs, 0)
+        input_bag = inputs  # torch.Size([1, 13, 40, 5])
+        inst_conv1 = self.conv1(input_bag)  # torch.Size([1, 16, 40, 5])
+        print(inst_conv1.shape)
+        inst_pool1 = self.pool1(inst_conv1)  # torch.Size([1, 16, 40, 5])
+        # inst_pool1 = torch.squeeze(inst_pool1, 0)  # torch.Size([16, 40, 2])
+        print(inst_pool1.shape)
+
+        embedding = self.lstm(inst_pool1)  # torch.Size([13, 16, 32])
+        # print(embedding.shape)
+
+        attention_v = self.attention_v(embedding)
+        attention_u = self.attention_v(embedding)
+
+        # print(attention_u.shape, attention_v.shape)
+        gated_attention = self.attention_weights(attention_u * attention_v).permute((1, 0))
+
+        gated_attention = nn.Softmax()(gated_attention)  # torch.Size([1, 13])
+
+        bag_features = torch.matmul(gated_attention, embedding)
+        # print(embedding.shape, gated_attention.shape)
+        bag_probability = self.cls(bag_features)
+
+        return bag_probability, gated_attention
+
+
 if __name__ == '__main__':
     x = torch.rand((1, 13, 40, 5))
-    encoder = WeakRM()
+    encoder = WeakRMLSTM()
     summary(encoder, x)
--- train.py
+++ train.py
@@ -7,6 +7,8 @@ from torch.utils.data import DataLoader, Dataset
 import torch.optim as optim
 from torch.optim import lr_scheduler
 import torch.nn.utils as utils
+from tqdm import tqdm
+from model import WeakRM
 
 
 class LibriSamples(torch.utils.data.Dataset):
@@ -31,7 +33,7 @@ train_cls_path = "data/train_clsname_demo.npy"
 valid_data_path = "data/valid_bags_demo.npy"
 valid_cls_path = "data/valid_clsname_demo.npy"
 
-batch_size = 32
+batch_size = 1
 # print(len(np.load(train_cls_path)))
 # print(len(np.load(train_data_path)))
 # print(len(np.load(valid_cls_path)))
@@ -46,15 +48,51 @@ valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=batch_size, sh
 print(len(train_loader))
 print(len(valid_loader))
 
-for data in train_loader:
-    x, y = data
-    print(x)
-    print(y)
-    print(x.shape, y.shape)
-    break
-# for data in valid_loader:
+# for data in train_loader:
 #     x, y = data
 #     print(x)
 #     print(y)
 #     print(x.shape, y.shape)
 #     break
+
+model = WeakRM()
+epochs = 3
+lr = 1e-3
+optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=1e-4)
+criterion = nn.CrossEntropyLoss()
+
+for epoch in range(1, epochs + 1):
+    batch_bar = tqdm(total=len(train_loader), dynamic_ncols=True, leave=False, position=0, desc='Train')
+
+    num_correct = 0
+    total_loss = 0
+
+    # training samples
+    # model.train()
+    for i, (x, y) in enumerate(train_loader):
+        optimizer.zero_grad()
+
+        x = x
+        y = y
+
+        # Don't be surprised - we just wrap these two lines to make it work for FP16
+        outputs = model(x)
+        loss = criterion(outputs, y)
+
+        num_correct += int((torch.argmax(outputs) == y).sum())
+        total_loss += float(loss)
+
+        # tqdm lets you add some details so you can monitor training as you train.
+        batch_bar.set_postfix(
+            acc="{:.04f}%".format(100 * num_correct / ((i + 1) * batch_size)),
+            loss="{:.04f}".format(float(total_loss / (i + 1))),
+            num_correct=num_correct,
+            lr="{:.04f}".format(float(optimizer.param_groups[0]['lr'])))
+
+        # Another couple things you need for FP16.
+        loss.backward()  # This is a replacement for loss.backward()
+        optimizer.step()  # This is a replacement for optimizer.step()
+
+        batch_bar.update()  # Update tqdm bar
+    batch_bar.close()  # You need this to close the tqdm bar
+
