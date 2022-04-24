d5a6bad2d9e3ecea328d93dea251bbb550c63d51
yuxuanwu17 <yuxuan.wu17@gmail.com>
2022-04-24 13:24:54 -0400

    finish the updated of weakRM, correct version

--- README.md
+++ README.md
@@ -2,5 +2,44 @@
 
 # TODO
 - modify the one hot encoding, excluding the N symbol in the sequence
-
 - Build the model for classification (embedding part)
+- Find the reason for fluctuation
+
+
+
+
+
+
+
+# Idea 
+
+In genomics, weakly supervised learning, especially multi-instance learning (MIL), has been intensively applied for studying protein–DNA interaction (Gao and Ruan, 2015, 2017; Zhang et al., 2019, 2020), with the basic assumption that `the sequences captured by CLIP (or ChIP-seq) technologies contain both the interacting and non-interacting elements with the proteins`. We know only the label of the entire sequence, but it is not exactly clear which part of the sequence plays the key role, and a significant proportion of it may not contribute to the binding between DNA and protein at all. 
+
+
+
+# Model explanation
+
+==How to obtain bag level probabilities from the instance level features without instance level labels==
+
+
+
+- MAX: extract information only concerning the most favored instance (overlooks other valuable instances, suffer from the outliers) `MaxPooling`
+- Average pooling: assign equal weights to all the instances, ignoring the fact that instances are sparsely distributed. `Average pooling`
+- Noisy and method: Not learnable
+- Our methods `Gated attention methods`: learnable weights
+
+
+
+## Gated attention
+
+- A feature merging approach
+- Use a three layer neural network to learn weights $a_k$ of the low dimensional representation of each instance and obtains the bag level embedding according to the equation $Z=\sum_{k=1}^{K}a_kh_k$ where h is a bag of K instance feature 
+- A tanh and sigmoid to learn the nonlinearity information 
+- A fully connected layer then takes the element-wise multiplication of two non-linearizes and return the gated attention weights for each instance, where the parameters could be learnable
+
+
+
+Attention would measure the degree of similarity among instances and thus is a suitable for our context-dependent data.
+
+
+
--- model.py
+++ model.py
@@ -6,28 +6,84 @@ import matplotlib.pyplot as plt
 from torchsummaryX import summary
 
 
+# class WeakRM(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.embedding = nn.Sequential(
+#             nn.Conv1d(40, 32, kernel_size=15, padding=7, stride=1),
+#             nn.ReLU(),
+#             nn.MaxPool1d(2),
+#             nn.Dropout(0.25),
+#             nn.Conv1d(32, 16, kernel_size=5, padding=2, stride=1),
+#             nn.ReLU(),
+#             nn.Dropout(0.25),
+#             nn.Flatten()
+#         )
+#
+#         self.attention_v = nn.Sequential(
+#             nn.Linear(32, 128),
+#             nn.Tanh()
+#         )
+#
+#         self.attention_u = nn.Sequential(
+#             nn.Linear(32, 128),
+#             nn.Sigmoid()
+#         )
+#
+#         self.attention_weights = nn.Sequential(
+#             nn.Linear(128, 1),
+#             nn.Softmax()
+#         )
+#
+#         self.cls = nn.Sequential(
+#             nn.Linear(32, 1),
+#             nn.Sigmoid()
+#         )
+#
+#     def forward(self, inputs, training=True, mask=None):
+#         inputs = torch.squeeze(inputs, 0)
+#
+#         embedding = self.embedding(inputs)  # torch.Size([13, 32])
+#         # print(embedding.shape)
+#
+#         attention_v = self.attention_v(embedding)
+#         attention_u = self.attention_v(embedding)
+#
+#         # print(attention_u.shape, attention_v.shape)
+#         gated_attention = self.attention_weights(attention_u * attention_v).permute((1, 0))
+#
+#         gated_attention = nn.Softmax()(gated_attention)  # torch.Size([1, 13])
+#
+#         bag_features = torch.matmul(gated_attention, embedding)
+#         # print(embedding.shape, gated_attention.shape)
+#         bag_probability = self.cls(bag_features)
+#
+#         return bag_probability, gated_attention
 class WeakRM(nn.Module):
-    def __init__(self):
+    def __init__(self, training=True):
         super().__init__()
 
-        self.embedding = nn.Sequential(
-            nn.Conv1d(40, 32, kernel_size=15, padding=7, stride=1),
+        self.inst_conv1 = nn.Sequential(
+            nn.Conv1d(4, 32, kernel_size=15, padding=7, stride=1),
             nn.ReLU(),
             nn.MaxPool1d(2),
-            nn.Dropout(0.25),
-            nn.Conv1d(32, 16, kernel_size=5, padding=2, stride=1),
-            nn.ReLU(),
-            nn.Dropout(0.25),
-            nn.Flatten()
         )
 
+        self.inst_conv2 = nn.Sequential(
+            nn.Conv1d(32, 16, kernel_size=5, padding=2, stride=1),
+            nn.ReLU()
+        )
+
+        self.dropout = nn.Dropout(0.2)
+
         self.attention_v = nn.Sequential(
-            nn.Linear(32, 128),
+            nn.Linear(320, 128),
             nn.Tanh()
         )
 
         self.attention_u = nn.Sequential(
-            nn.Linear(32, 128),
+            nn.Linear(320, 128),
             nn.Sigmoid()
         )
 
@@ -37,26 +93,44 @@ class WeakRM(nn.Module):
         )
 
         self.cls = nn.Sequential(
-            nn.Linear(32, 1),
+            nn.Linear(320, 1),
             nn.Sigmoid()
         )
+        self.softmax = nn.Softmax()
 
     def forward(self, inputs, training=True, mask=None):
         inputs = torch.squeeze(inputs, 0)
+        print(inputs.shape)
+        inputs = inputs.permute((0, 2, 1))
+        print(inputs.shape)
+        inst_conv1 = self.inst_conv1(inputs)  # torch.Size([13, 32, 20])
+        print(inst_conv1.shape)
+        if training:
+            inst_conv1 = self.dropout(inst_conv1)
 
-        embedding = self.embedding(inputs)  # torch.Size([13, 32])
-        print(embedding.shape)
+        inst_conv2 = self.inst_conv2(inst_conv1)
+        print(inst_conv2.shape)
 
-        attention_v = self.attention_v(embedding)
-        attention_u = self.attention_v(embedding)
+        inst_features = nn.Flatten()(inst_conv2)
+        print(inst_features.shape)  # torch.Size([13, 320])
 
-        print(attention_u.shape, attention_v.shape)
+        attention_v = self.attention_v(inst_features)
+        attention_u = self.attention_v(inst_features)
+
+        print(attention_u.shape)
+        print(attention_v.shape)
+
+        # print(attention_u.shape, attention_v.shape)
         gated_attention = self.attention_weights(attention_u * attention_v).permute((1, 0))
+        print(gated_attention.shape)
 
-        gated_attention = nn.Softmax()(gated_attention)  # torch.Size([1, 13])
+        gated_attention = self.softmax(gated_attention)  # torch.Size([1, 13])
+        print("gated attention shape", gated_attention.shape)
+        print("inst features shape", inst_features.shape)
 
-        bag_features = torch.matmul(gated_attention, embedding)
-        print(embedding.shape, gated_attention.shape)
+        bag_features = torch.matmul(gated_attention, inst_features)
+        print(bag_features.shape)
+
         bag_probability = self.cls(bag_features)
 
         return bag_probability, gated_attention
@@ -101,11 +175,11 @@ class WeakRMLSTM(nn.Module):
         inst_conv1 = self.conv1(input_bag)  # torch.Size([1, 16, 40, 5])
         print(inst_conv1.shape)
         inst_pool1 = self.pool1(inst_conv1)  # torch.Size([1, 16, 40, 5])
-        # inst_pool1 = torch.squeeze(inst_pool1, 0)  # torch.Size([16, 40, 2])
+        inst_pool1 = torch.squeeze(inst_pool1, 0)  # torch.Size([16, 40, 2])
         print(inst_pool1.shape)
 
         embedding = self.lstm(inst_pool1)  # torch.Size([13, 16, 32])
-        # print(embedding.shape)
+        print(embedding.shape)
 
         attention_v = self.attention_v(embedding)
         attention_u = self.attention_v(embedding)
@@ -122,7 +196,51 @@ class WeakRMLSTM(nn.Module):
         return bag_probability, gated_attention
 
 
+class WSCNN(nn.Module):
+
+    def __init__(self, instance_len=40, merging='MAX', ):
+        super(WSCNN, self).__init__()
+
+        assert merging in ['MAX', 'AVG']
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(13, 16, kernel_size=(1, 15), padding=(0, 7)),
+            nn.ReLU()
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=(1, 1), padding=(0, 7)),
+            nn.ReLU()
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=(1, 1), padding=(0, 7)),
+            nn.ReLU()
+        )
+        self.dropout = nn.Dropout(0.5)
+        self.pool1 = nn.MaxPool2d((1, instance_len))
+
+        if merging == 'MAX':
+            self.pool2 = nn.AdaptiveMaxPool2d((1, 1))
+            # self.pool2 = nn.GlobalMaxPooling2d()
+        elif merging == 'AVG':
+            self.pool2 = nn.AdaptiveAvgPool2d((1, 1))
+            # self.pool2 = nn.GlobalAveragePooling2d()
+
+    def forward(self, inputs, training=True, mask=None):
+        x = self.conv1(inputs)
+        print(x.shape)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        print(x.shape)
+        if training:
+            x = self.dropout(x, training=training)
+        x = self.conv3(x)
+        out = self.pool2(x)
+        return out
+
+
 if __name__ == '__main__':
-    x = torch.rand((1, 13, 40, 5))
-    encoder = WeakRMLSTM()
+    x = torch.rand((1, 13, 40, 4))
+    encoder = WeakRM()
+    # encoder = WeakRMLSTM()
+    # encoder = WSCNN()
     summary(encoder, x)
